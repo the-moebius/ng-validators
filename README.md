e51b6b2a0de118a10883e782efb3b7e2d4d133ed
Slava Fomin II <s.fomin@betsol.ru>
2017-05-19 21:08:19 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# Collection of input validators for Angular 2+
+
+[![npm version](https://badge.fury.io/js/%40moebius-mlm%2Fng-validators.svg)](https://badge.fury.io/js/%40moebius-mlm%2Fng-validators)
+
+This repository contains several input validators which could be used
+independently or with Angular 2+.
+
+## Provided validators
+
+* Password
+
+
+## Support
+
+If this library is useful to you, please add a star on [GitHub repository][repo-gh].
+
+Thank you!
+
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Slava Fomin II, MOEBIUS FOUNDATION
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+
+  [repo-gh]: https://github.com/moebius-mlm/ng-validators
---
+++ src/validators/password.validator.ts
@@ -0,0 +1,71 @@
+
+import {FormControl} from '@angular/forms';
+
+
+export interface PasswordValidatorOptions {
+  minLength?: number;
+  maxLength?: number;
+  requireLowerCaseLetters?: boolean;
+  requireUpperCaseLetters?: boolean;
+  requireNumbers?: boolean;
+  requireSpecialCharacters?: boolean;
+}
+
+export function passwordValidator (options: PasswordValidatorOptions) {
+
+  const validator = new PasswordValidator(options);
+
+  return function validatePassword (control: FormControl) {
+    return validator.validate(control.value);
+  }
+
+}
+
+export class PasswordValidator {
+
+  private lowerCaseLetterMatcher = /[a-z]+/;
+  private upperCaseLetterMatcher = /[A-Z]+/;
+  private numberMatcher = /[0-9]+/;
+  private specialCharactersMatcher = /[-+=_.,:;~`!@#$%^&*(){}<>\[\]"'\/\\]+/;
+
+
+  constructor (private options: PasswordValidatorOptions) {
+  }
+
+  public validate (value: any): any {
+
+    const errors: any = {};
+
+    if (this.options.minLength > 0 && value.length < this.options.minLength) {
+      errors.passwordMinLengthRequired = {
+        minLength: this.options.minLength
+      };
+    }
+
+    if (this.options.maxLength >= 0 && value.length > this.options.maxLength) {
+      errors.passwordMaxLengthExceeded = {
+        maxLength: this.options.maxLength
+      };
+    }
+
+    if (this.options.requireLowerCaseLetters && !this.lowerCaseLetterMatcher.test(value)) {
+      errors.passwordLowerCaseLetterRequired = true;
+    }
+
+    if (this.options.requireUpperCaseLetters  && !this.upperCaseLetterMatcher.test(value)) {
+      errors.passwordUpperCaseLetterRequired = true;
+    }
+
+    if (this.options.requireNumbers && !this.numberMatcher.test(value)) {
+      errors.passwordNumberRequired = true;
+    }
+
+    if (this.options.requireSpecialCharacters && !this.specialCharactersMatcher.test(value)) {
+      errors.passwordSpecialCharacterRequired = true;
+    }
+
+    return Object.keys(errors).length > 0 ? errors : null;
+
+  }
+
+}
