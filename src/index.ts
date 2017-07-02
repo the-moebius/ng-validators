c05c154d43c18545e6e3cbb02cf4bf457bc9ec22
Slava Fomin II <s.fomin@betsol.ru>
2017-07-02 18:38:33 +0300

    Introduced proper package structure.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+export {InputValidatorsModule} from './input-validators.module';
+
+export {birthDateValidator, BirthDateValidator, BirthDateValidatorOptions} from './validators/birth-date.validator';
+
+export {isTrueValidator} from './validators/is-true';
+
+export {matchOtherValidator} from './validators/match-other.validator';
+
+export {passwordValidator, PasswordValidator, PasswordValidatorOptions} from './validators/password.validator';
+
+export {skypeNameValidator, SkypeNameValidator} from './validators/skype-name.validator';
