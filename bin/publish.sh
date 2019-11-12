3b2510e6e53bd25d86bb6ad8d68c035330e3ef66
Slava Fomin II <slava@fomin.io>
2019-11-13 00:18:36 +0300

    Fixed publish config

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,12 +6,7 @@ set -o pipefail
 ROOT_PATH="$(dirname "$0")/.."
 
 # https://gist.github.com/DarrenN/8c6a5b969481725a4413
-PACKAGE_VERSION=$(cat "${ROOT_PATH}/package.json" \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g'
-)
+PACKAGE_VERSION=$(grep 'version' package.json | cut -d '"' -f4 | tr -d '[[:space:]]')
 
 # Building the package
 echo "Building the package…"
@@ -24,7 +19,7 @@ npm publish
 
 # Tagging the release in Git
 echo "Tagging the release…"
-git tag "${PACKAGE_VERSION}"
+git tag "v${PACKAGE_VERSION}"
 git push --tags
 
 echo "All done!"
